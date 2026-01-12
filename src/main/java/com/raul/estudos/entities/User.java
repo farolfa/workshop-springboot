fb739aee5d685019528cfb442ede1e9503a35038
farolfa <raulgoncalves.profissional@gmail.com>
2026-01-12 13:30:53 -0300

    Order, Instant, ISO - Part 1

--- src/main/java/com/raul/estudos/entities/User.java
+++ src/main/java/com/raul/estudos/entities/User.java
@@ -1,11 +1,14 @@
 package com.raul.estudos.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -17,10 +20,14 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String number;
     private String password;
+
+    @OneToMany(mappedBy="client")
+    private List<Order> orders=new ArrayList<>() ;
 
 
     public User() {
@@ -83,6 +90,16 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
     }
 
 
