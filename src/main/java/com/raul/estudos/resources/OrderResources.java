56209c2133130f39de58d8162fb9949965400ede
farolfa <raulgoncalves.profissional@gmail.com>
2026-02-27 13:35:56 -0300

    inserted the RequestBody annotation

--- src/main/java/com/raul/estudos/resources/OrderResources.java
+++ src/main/java/com/raul/estudos/resources/OrderResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,10 +35,10 @@ public class OrderResources {
     }
 
     @PostMapping
-    public ResponseEntity<Order> save(Order u){
+    public ResponseEntity<Order> save(@RequestBody Order u){
         return ResponseEntity.ok().body(services.save(u));
     }
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete (@PathVariable Long id){
         return ResponseEntity.noContent().build();
     }
