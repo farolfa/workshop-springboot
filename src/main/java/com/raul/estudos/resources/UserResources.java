fbe33ddfeafff98a772a647a4848a6b23de91c26
farolfa <raulgoncalves.profissional@gmail.com>
2026-02-27 13:41:01 -0300

    minor fix RequestMapping para RequestBody no UserResource

--- src/main/java/com/raul/estudos/resources/UserResources.java
+++ src/main/java/com/raul/estudos/resources/UserResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,7 +34,7 @@ public class UserResources {
         return ResponseEntity.ok().body(obj);
     }
     @PostMapping
-    public ResponseEntity<User> save(@RequestMapping User u){
+    public ResponseEntity<User> save(@RequestBody User u){
         return ResponseEntity.ok().body(services.save(u));
     }
     @DeleteMapping("/{id}")
